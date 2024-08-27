05719d7e97fbfe742eb472e4875ad9759d35d115
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-08-27 15:17:26 +0200

    add header, style

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { GameComponent } from '../game/game.component';
 import { GamesListComponent } from '../games-list/games-list.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [GameComponent, GamesListComponent],
+  imports: [GameComponent, GamesListComponent, HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })

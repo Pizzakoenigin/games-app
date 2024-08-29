3dfb07c34fa2f656cd9b3c4484ced502cf334241
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-08-29 14:27:31 +0200

    style

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -3,17 +3,27 @@ import { RawgService } from '../../services/rawg.service';
 import { Game } from '../../interfaces/game';
 import { HeaderComponent } from '../header/header.component';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [HeaderComponent, CommonModule],
+  imports: [HeaderComponent,
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink
+  ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
-export class GameComponent implements OnInit{
+export class GameComponent implements OnInit {
   game: any = [
-    
+
   ]
 
   constructor(public rawgService: RawgService) {
